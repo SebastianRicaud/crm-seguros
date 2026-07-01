3d187af1df4c64148872e211b7d30a41461ce8aa
SebastianRicaud <ricaudsebastian@gmail.com>
2026-07-01 20:37:31 -0300

    Fix: Add icon to policies KPI

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -328,7 +328,11 @@ export function Dashboard() {
         <KPICard 
           label="Pólizas en cartera" 
           value={stats.policies} 
-          icon=""
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7 text-blue-600">
+              <path fillRule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625zM7.5 15a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 017.5 15zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H8.25z" clipRule="evenodd" />
+            </svg>
+          }
           color="bg-blue-50"
           iconColor="text-blue-600"
           borderColor="border-slate-400"
